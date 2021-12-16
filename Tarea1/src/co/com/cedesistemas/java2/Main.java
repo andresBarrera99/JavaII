bdf9078950dbdf4e446778bbc7a03a8ca03dac72
Jonathan Barrera <andres.barar@gmail.com>
2021-12-15 22:21:58 -0500

    Creacion de menu de usuarios

--- Tarea1/src/co/com/cedesistemas/java2/Main.java
+++ Tarea1/src/co/com/cedesistemas/java2/Main.java
@@ -24,15 +24,20 @@ public class Main {
             String s = in.nextLine();
             switch (s){
                 case "1":
+                    int contador = 1;
                     for(Pizza pizza : pizzas) {
-                        System.out.print("1: ");
+                        System.out.print(contador++ + ": ");
                         System.out.println(pizza.getDescripcion());
                     }
                     break;
                 case "2":
                     System.out.println("ingrese el código de las pizzas a comprar separado por coma (1,2) sin espacios");
                     String pizzasPorComprar = in.nextLine();
-
+                    if (!"".equals(pizzasPorComprar.trim())) {
+                        String response = procesarPizzas(pizzasPorComprar, pizzas,in);
+                        System.out.println(response);
+                    }else
+                        System.out.println("No seleccionó pizzas");
                     break;
                 case "3":
                     System.out.println("Finalizando...");
@@ -44,6 +49,32 @@ public class Main {
 
             }
         }
+    }
+
+    private static String procesarPizzas(String pizzasPorComprar, List<Pizza> pizzasDisponibles,Scanner in) {
+        int total =0;
+        String[] pizzas = pizzasPorComprar.split(",");
+        for (String pizza : pizzas){
+            int idx = Integer.parseInt(pizza);
+            if (idx == 0 || idx > pizzasDisponibles.size())
+                return "Una de las pizzas seleccionadas no está dentro de menu";
+            total += pizzasDisponibles.get(idx -1).getPrecio();
+        }
+        String continuar;
+        String response = "";
+        do {
+            System.out.println("El total es :" + total);
+            System.out.print("¿Desea continuar?(S-N): ");
+            continuar = in.nextLine();
+
+            if ("S".equalsIgnoreCase(continuar))
+                response = "Pizzas compradas con éxito";
+            else if ("N".equalsIgnoreCase(continuar))
+                response = "Canceló la compra";
+            else
+                System.out.println("Ingrese una opcion válida");
+        }while (!"S".equalsIgnoreCase(continuar) || "N".equalsIgnoreCase(continuar));
+        return response;
     }
 
 }
